f9dc3499b4a58df9ab6ddcbce2a3495bd5056345
chrisc9878 <33165542+chrisc9878@users.noreply.github.com>
2018-03-21 14:57:21 -0400

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    public class Game
+    {
+        //Properties 
+        public string team1Name { get; set; }
+        public string team2Name { get; set; }
+        public int team1score { get; set; }
+        public int team2score { get; set; }
+
+        //Default constructor
+        public Game()
+        {
+
+        }
+
+        //Constructor with 4 parameters that initializes the class properties from the parameter values
+        public Game(string team1, int team1score, string team2, int team2score)
+        {
+            this.team1Name = team1;
+            this.team1score = team1score;
+            this.team2Name = team2;
+            this.team2score = team2score;
+        }
+
+        //Add a public ToString method to  display the info of the teams 
+        public override string ToString()
+        {
+            //use string.format method
+            return string.Format("{0} ({1}) - {2} ({3})",
+                team1Name, team1score, team2Name, team2score);
+        }
+    }
+}
---
+++ GameFactory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+namespace GameLibrary
+{
+    public class GameFactory
+    {
+        StreamWriter sw;
+        XmlSerializer serial;
+        List<Game> GameSchedule;
+        const string Game_Data = @"..\..\games.xml";
+
+        public void CreateGameList()
+        {
+            GameSchedule = new List<Game>();
+
+            Game g = new Game("Miami Heat", 106, "New York Knicks", 98);
+            GameSchedule.Add(g);
+
+            g = new Game("Portland Trailblazers", 123, "New Jersey Nets", 105);
+            GameSchedule.Add(g);
+
+            g = new Game("Saints", 24, "Vikings", 29);
+            GameSchedule.Add(g);
+
+            g = new Game("Jaguars", 45, "Steelers", 42);
+            GameSchedule.Add(g);
+
+            g = new Game("Red Sox", 7, "Yankees", 11);
+            GameSchedule.Add(g);
+
+            g = new Game("Marlins", 6, "Cubs", 12);
+            GameSchedule.Add(g);
+
+            serial = new XmlSerializer(GameSchedule.GetType());
+            sw = new StreamWriter(Game_Data);
+            serial.Serialize(sw, GameSchedule);
+            sw.Close();
+        }
+
+        public List<Game> De_Serialize()
+        {
+            GameSchedule = new List<Game>();
+            StreamReader sr = new StreamReader(Game_Data);
+            serial = new XmlSerializer(GameSchedule.GetType());
+            GameSchedule = (List<Game>)serial.Deserialize(sr);
+            sr.Close();
+            return GameSchedule;
+        }
+    }
+}
